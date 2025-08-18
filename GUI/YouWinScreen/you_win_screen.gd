5067c62f7bf907af394886c0475eb72b2ea48a4f
Shane Huntoon <Shuntoon555@gmail.com>
2025-08-18 14:21:45 -0400

    Added You Win Screen and Level Manager

---
+++ GUI/YouWinScreen/you_win_screen.gd
@@ -0,0 +1,27 @@
+extends Control
+
+var level_manager : LevelManager
+
+func _ready() -> void:
+	level_manager = get_tree().get_first_node_in_group("level_manager")
+	level_manager.connect("level_finished", _on_level_finished)
+
+func _on_restart_button_pressed() -> void:
+	get_tree().reload_current_scene()
+	pass # Replace with function body.
+
+
+func _on_continue_button_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_main_menu_butoon_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_level_finished() -> void:
+	visible = true
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	var tween = create_tween()
+	tween.tween_property(self, "position", Vector2(0,-1000), 0)
+	tween.tween_property(self, "position", Vector2(0,0), .5)
--- Objects/FinishPoint/finish_point.gd
+++ Objects/FinishPoint/finish_point.gd
@@ -1,5 +1,10 @@
 extends Node3D
 
+var level_manager : LevelManager
+
+func _ready() -> void:
+	level_manager = get_tree().get_first_node_in_group("level_manager")
+
 func _on_finish_area_body_entered(body):
 	if body.owner.is_in_group("player"):
 		var camera = body.owner.get_node("CameraContainer")
@@ -7,4 +12,6 @@ func _on_finish_area_body_entered(body):
 		camera.level_finished = true
 		body.can_move = false
 		body.level_finished = true
+		
+		level_manager.level_finished.emit()
 	pass # Replace with function body.
