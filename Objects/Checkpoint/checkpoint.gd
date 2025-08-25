05d9f8b357f4e6e2619c09e48004dd5d90e9b3de
Shane Huntoon <Shuntoon555@gmail.com>
2025-08-25 15:42:56 -0400

    Added Scripts

---
+++ Objects/Checkpoint/checkpoint.gd
@@ -0,0 +1,11 @@
+extends Node3D
+class_name Checkpoint
+
+@onready var collision_shape_3d: CollisionShape3D = $Area3D/CollisionShape3D
+@onready var spawn_point: Marker3D = $SpawnPoint
+@onready var level_manager: Node = %LevelManager
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	level_manager.checkpoint_reached.emit(spawn_point.global_position)
+	collision_shape_3d.set_deferred("disabled",true)
+	pass # Replace with function body.
--- Objects/LevelManager/level_manager.gd
+++ Objects/LevelManager/level_manager.gd
@@ -2,9 +2,15 @@ extends Node
 class_name LevelManager
 
 signal level_finished
+signal checkpoint_reached(position : Vector3)
 
 var spawn_location : Vector3
 @onready var player: Node3D = %Player
 
 func _ready() -> void:
 	spawn_location = player.global_position
+
+
+func _on_checkpoint_reached(position) -> void:
+	spawn_location = position
+	pass # Replace with function body.
