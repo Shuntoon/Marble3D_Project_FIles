4eb359c9f11845938b0b0713cf1acdfe2a280834
Shane Huntoon <Shuntoon555@gmail.com>
2025-10-31 13:56:00 -0400

    Added Coin

--- Objects/Checkpoint/checkpoint.gd
+++ Objects/Checkpoint/checkpoint.gd
@@ -6,6 +6,7 @@ class_name Checkpoint
 @onready var level_manager: Node = %LevelManager
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	level_manager.checkpoint_reached.emit(spawn_point.global_position)
-	collision_shape_3d.set_deferred("disabled",true)
+	if body.owner.is_in_group("player"):
+		level_manager.checkpoint_reached.emit(spawn_point.global_position)
+		collision_shape_3d.set_deferred("disabled",true)
 	pass # Replace with function body.
